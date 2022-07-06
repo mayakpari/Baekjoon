f1fb821eb886d9003f026d9faba35ecd578309f2
MAYAKPARI <66272331+mayakpari@users.noreply.github.com>
2022-07-06 23:44:30 +0900

    [Bronze V] Title: 문자열, Time: 116 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test_case = Integer.parseInt(br.readLine());
+		for(int t=0;t<test_case;t++) {
+			String str = br.readLine();
+			System.out.print(str.charAt(0));
+			System.out.println(str.charAt(str.length()-1));
+		}
+	}
+
+}
