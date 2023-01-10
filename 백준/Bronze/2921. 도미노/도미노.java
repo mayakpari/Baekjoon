e0bc0e965ec12d4fc734cae75c27c49016d38ad9
MAYAKPARI <66272331+mayakpari@users.noreply.github.com>
2023-01-10 23:35:27 +0900

    [Bronze III] Title: 도미노, Time: 156 ms, Memory: 15784 KB -BaekjoonHub

---
+++ 백준/Bronze/2921. 도미노/도미노.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import javax.swing.text.AbstractDocument.BranchElement;
+import java.io.*;
+ 
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+        int n= Integer.parseInt(br.readLine());       
+        int sum =0;
+        for(int i = 0; i <= n; i++){
+            for(int j = i; j <= n; j++){
+                sum = sum + i + j;
+            }
+        }
+        bw.write(sum+"\n");
+        bw.flush();
+        bw.close();
+}
+}
