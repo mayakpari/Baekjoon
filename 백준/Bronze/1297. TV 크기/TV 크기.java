3d23fbac75a532fafcf8cf501aa1b610ac987f3f
MAYAKPARI <66272331+mayakpari@users.noreply.github.com>
2023-01-19 23:55:15 +0900

    [Bronze II] Title: TV 크기, Time: 236 ms, Memory: 18516 KB -BaekjoonHub

---
+++ 백준/Bronze/1297. TV 크기/TV 크기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int D = sc.nextInt();
+		int H_rate = sc.nextInt();
+		int W_rate  = sc.nextInt();
+		double answer = Math.sqrt(1.0*D*D/(H_rate*H_rate+W_rate*W_rate));
+		System.out.println((int) Math.floor(answer*H_rate)+" "+(int) Math.floor(answer*W_rate));
+		sc.close();
+	}    
+}
