f20d0e8db512f0e4a81962b82753c202ab867e8b
MAYAKPARI <66272331+mayakpari@users.noreply.github.com>
2023-03-15 20:40:22 +0900

    [unrated] Title: 덧칠하기, Time: 2.59 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -2,18 +2,18 @@ class Solution {
     public int solution(int n, int m, int[] section) {
         
         int answer = 0;
-        boolean[] board = new boolean[n+1];
+        boolean[] wall = new boolean[n+1];
         
         for(int i=0;i<section.length;i++){
-            board[section[i]] = true; // 칠해줘야 하는 부분을 true처리
+            wall[section[i]] = true; // 칠해줘야 하는 부분을 true처리
         }
         
         
         for(int i=0;i<section.length;i++){
-            if(board[section[i]]){ // 칠해줘야 한다면
+            if(wall[section[i]]){ // 칠해줘야 한다면
                 for(int j=section[i];j<section[i]+m;j++){ // 해당위치를 기준으로 롤러크기만큼 false 처리
-                    if(j<n+1){
-                        board[j] = false;
+                    if(j<n+1){ // 배열 범위 초과 검사
+                        wall[j] = false;
                     }
                 }
                 answer++;
