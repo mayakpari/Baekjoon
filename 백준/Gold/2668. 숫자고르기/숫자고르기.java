71321112db71d7b1dab6512e4537e18d3a82ad04
MAYAKPARI <66272331+mayakpari@users.noreply.github.com>
2023-03-31 10:29:56 +0900

    [Gold V] Title: 숫자고르기, Time: 216 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    static int n;
+    static int[] arr;
+    static boolean[] check;
+    static List<Integer> list;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        arr = new int[n+1];
+        check = new boolean[n+1];
+        list = new ArrayList<>();
+
+        for(int i=1;i<=n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=1;i<=n;i++){
+            check[i] = true;
+            dfs(i,i);
+            check[i] = false;
+        }
+
+        Collections.sort(list);
+        System.out.println(list.size());
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+    }
+
+    public static void dfs(int start, int end){
+        if(!check[arr[start]]){
+            check[arr[start]] = true;
+            dfs(arr[start],end);
+            check[arr[start]] = false;
+        }
+        
+        if(arr[start] == end){
+            list.add(end);
+        }
+    }
+}
