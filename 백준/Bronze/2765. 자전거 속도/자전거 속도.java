0e1a6fb4178d76fb2b7b4f3f08f1212621b888e6
MAYAKPARI <66272331+mayakpari@users.noreply.github.com>
2024-05-15 23:49:53 +0900

    [Bronze III] Title: 자전거 속도, Time: 272 ms, Memory: 19324 KB -BaekjoonHub

---
+++ 백준/Bronze/2765. 자전거 속도/자전거 속도.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    static double pi = 3.1415927;
+    static int mileToFeet = 5280;
+    static int feetToInch = 12;
+    static int hourToMinute = 60;
+    static int MinuteToSec = 60;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = 1;
+        while (true) {
+            double distance = 0;
+            double mph = 0;
+
+            double d = sc.nextDouble();
+            int c = sc.nextInt();
+            double s = sc.nextDouble();
+
+            if (c == 0)
+                break;
+
+            distance = (d*pi*c) / (mileToFeet * feetToInch);
+            mph = distance* MinuteToSec * hourToMinute / s;
+            System.out.println("Trip #" + N + ": " + String.format("%.2f", distance) +
+                    " " + String.format("%.2f", mph));
+            N++;
+        }
+        sc.close();
+    }
+}
