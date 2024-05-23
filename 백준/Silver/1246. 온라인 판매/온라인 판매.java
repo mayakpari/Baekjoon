a29c016a5e07b13182aa4759400b50c07e80661f
MAYAKPARI <66272331+mayakpari@users.noreply.github.com>
2024-05-23 23:52:47 +0900

    [Silver IV] Title: 온라인 판매, Time: 168 ms, Memory: 16076 KB -BaekjoonHub

---
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st =  new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M =Integer.parseInt(st.nextToken());
+		
+		ArrayList<Integer> arr = new ArrayList<>();
+		
+		for (int i = 0; i < M; i++) {
+			arr.add(Integer.parseInt(br.readLine()));
+		}
+		
+		Collections.sort(arr);
+		
+		int ans = 0;
+		int max = 0;
+		
+		for (int i = 0; i < M; i++) {
+			int tmp = arr.get(i);
+			int tmpsum = 0;
+			
+			if(M-i<N) {
+				tmpsum = tmp * (M-i);
+			}else {
+				tmpsum = tmp * N;
+			}
+			
+			if(tmpsum>max) {
+				max = tmpsum;
+				ans = tmp;
+			}
+		}
+		System.out.println(ans + " "+ max);
+	}
+}
