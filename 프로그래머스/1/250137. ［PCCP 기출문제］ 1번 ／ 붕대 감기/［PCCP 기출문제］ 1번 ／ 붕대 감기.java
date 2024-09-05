93c82cc1cc594e8c7a3ce713070a39ff88f54371
MAYAKPARI <66272331+mayakpari@users.noreply.github.com>
2024-09-05 19:23:14 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] bandage, int health, int[][] attacks) {
+        int answer = 0;
+        int curr = 0;
+        int time = 0;
+        int currHealth = health;
+        for(int i=0;i<attacks.length;i++){
+            time = attacks[i][0] - curr - 1; // 몬스터의 공격을 받기까지 걸린 시간
+            currHealth += time * bandage[1] + time/bandage[0] * bandage[2]; // 그 동안 회복한 체력
+            if(currHealth >= health){ // 초기 체력보다 많아지면 초기체력으로 조정
+                currHealth = health;
+            }
+            currHealth -= attacks[i][1]; // 공격으로 체력 감소
+            if(currHealth <= 0){ // 0 이하로 떨어지면 종료
+                return -1;
+            }
+            curr = attacks[i][0]; // 현재 시간 조정
+        }
+        answer = currHealth;
+        return answer;
+    }
+}
